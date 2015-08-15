b18ea557a8cee9d997fc1c660e4acffafd53d615
ishanuc <ishanu.chattopadhyay@gmail.com>
2015-08-15 02:55:45 -0500

    code updated

--- code/fitt.py
+++ code/fitt.py
@@ -1,9 +1,5 @@
 #!/usr/bin/python
-# code for estimating solution curve for classification  problems
-# USAGE: ./fitt.py [filename] [outfilename]
-# [filename] has two columns x y , x is the data amount, y is mutual information
-# produces multiple output files with [outfilename] as prefix
-#------------------------------------
+
 import os
 import inspect
 
@@ -15,14 +11,12 @@ import matplotlib as mpl
 import matplotlib.pyplot as plt
 
 def __residual(params, y, x):
-    A, B, P = params
+    A, P, dd = params
     EP=0
-    if P > 1:
-        EP=10
-    if B < 1:
+    if P < 1:
         EP=10
     
-    return y + A+ B/(x**P)  + EP
+    return np.log(y) + A/(x**P) - dd + EP
 
 OUTFILE='D'
 DATFILE='cpos'
@@ -38,11 +32,11 @@ x=np.array(x)
 y=np.array(y)
 
 #initial values
-A=1
-B=1
+A=12
 P=1
+dd=0
 
-p_opt,infodict = leastsq(__residual,  array([A,B,P]), args=(y, x),maxfev=1000000)
+p_opt,infodict = leastsq(__residual,  array([A,P,dd]), args=(y, x),maxfev=1000000)
 
 print 'output params', p_opt
 print 'message ( 1,2,3,4 => soln found) ', infodict
@@ -52,9 +46,9 @@ print 'ERRS=',ERR_
 print 'sum of lsq.= ', np.sum(np.abs(ERR_))
 
 A, P, dd =p_opt 
-plt.semilogx(x,2**(y-A),'ro')
+plt.semilogx(x,y/np.exp(dd),'ro')
 xx=np.logspace(0,2,100)
-plt.semilogx(xx,2**(-B/(xx**P)),'-k')
+plt.semilogx(xx,np.exp(-A/(xx**P)),'-k')
 
 if OUTFILE!="D":
     y1=y/np.exp(dd)
